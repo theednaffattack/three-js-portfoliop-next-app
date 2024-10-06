0e0d5241a1581f7c1556a487b6ea51ed30ce7206
Eddie Naff <eddienaff@gmail.com>
2024-10-05 19:53:58 -0700

    Add lights

--- src/components/render-model.tsx
+++ src/components/render-model.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { ReactChildren } from "@/types";
+import { Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import clsx from "clsx";
 import { Suspense } from "react";
@@ -11,6 +12,7 @@ export function RenderModel({ children, className }: RenderModelType) {
     <>
       <Canvas className={clsx("w-screen h-screen -z-10 relative", className)}>
         <Suspense fallback={null}>{children}</Suspense>
+        <Environment preset="dawn" />
       </Canvas>
     </>
   );
