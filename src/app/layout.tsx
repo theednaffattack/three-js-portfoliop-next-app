deff72ae62342a8d6f28e8ccf4edb72ae0915725
Eddie Naff <eddienaff@gmail.com>
2024-10-06 19:14:19 -0700

    Add next-extra for metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import clsx from "clsx";
 import "./globals.css";
+import { headers } from "next/headers";
+import { pathname } from "next-extra/pathname";
+
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
 export const metadata: Metadata = {
   title: "ThreeJS Portfolio Project",
@@ -15,9 +22,21 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const headersList = headers();
+  const activePath = headersList.get("x-invoke-path");
+  const headerValues = headersList.values();
+  const base = `${headers().get("x-forwarded-proto")}://${headers().get(
+    "host"
+  )}`;
+  const fullUrl = new URL(pathname(), base); // e.g., http://localhost:3000/some/path?a=1&b=2
+
+  console.log("ACTIVE PATH", { activePath, headerValues, fullUrl, base });
+
   return (
     <html lang="en">
       <body className={clsx(inter.variable, "bg-background text-foreground")}>
+        <div>What is pathname {activePath}</div>
+        <div>What is header values {headerValues}</div>
         {children}
       </body>
     </html>
